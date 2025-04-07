80f9cd4e172de6175336bb82bf6dfa65178049df
Stefan Thoss <git.identity164@slmails.com>
2025-04-07 15:16:38 -0700

    Remove imports

--- tests/test_charge_point_v16.py
+++ tests/test_charge_point_v16.py
@@ -40,16 +40,6 @@ from ocpp.v16.enums import (
     TriggerMessageStatus,
     UnlockStatus,
 )
-import pytest
-from pytest_homeassistant_custom_component.common import MockConfigEntry
-import websockets
-
-from custom_components.ocpp import async_setup_entry, async_unload_entry
-from custom_components.ocpp.button import BUTTONS
-from custom_components.ocpp.const import DOMAIN as OCPP_DOMAIN
-from custom_components.ocpp.enums import ConfigurationKey, HAChargerServices as csvcs
-from custom_components.ocpp.number import NUMBERS
-from custom_components.ocpp.switch import SWITCHES
 
 from .const import (
     MOCK_CONFIG_DATA,
